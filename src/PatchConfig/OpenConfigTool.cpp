e52153b78c35377b47596d4fa6131bfcf387e847
Zhang Boyang <zhangboyang.id@gmail.com>
2025-05-17 17:57:09 +0800

    20250517

--- src/PatchConfig/OpenConfigTool.cpp
+++ src/PatchConfig/OpenConfigTool.cpp
@@ -1,16 +1,15 @@
 #include "stdafx.h"
 
-#define title_MAX 5
-static const TCHAR title_CHS[] = _T("环境配置");
-static const TCHAR title_CHT[] = _T("設定程式");
+static const wchar_t wtitle_CHS[] = _T("环境配置");
+static const wchar_t wtitle_CHT[] = _T("設定程式");
+
+static char *title_CHS = NULL;
+static char *title_CHT = NULL;
 
 static BOOL CALLBACK SetForegroundIfMatched(HWND hwnd, LPARAM lParam)
 {
-	PROCESS_INFORMATION *pi = (PROCESS_INFORMATION *) lParam;
-	DWORD pid;
-	GetWindowThreadProcessId(hwnd, &pid);
-	TCHAR buf[title_MAX];
-	if (pid == pi->dwProcessId && !GetWindow(hwnd, GW_OWNER) && IsWindowVisible(hwnd) && GetWindowText(hwnd, buf, title_MAX) && (_tcscmp(buf, title_CHS) == 0 || _tcscmp(buf, title_CHT) == 0)) {
+	char buf[4096];
+	if (!GetWindow(hwnd, GW_OWNER) && IsWindowVisible(hwnd) && GetWindowTextA(hwnd, buf, sizeof(buf)) && (strcmp(buf, title_CHS) == 0 || strcmp(buf, title_CHT) == 0)) {
 		if (SetForegroundWindow(hwnd)) {
 			if (!is_win9x()) {
 				FLASHWINFO fwi;
@@ -36,6 +35,9 @@ void OpenConfigTool(CPatchConfigDlg *dlg)
 	dlg->SetTopMost(false);
 	ShowPleaseWaitDlg(dlg, STRTABLE(IDS_OPENCONFIGTOOLINPROGRESS));
 
+	if (!title_CHS) title_CHS = wcs2cs_alloc(wtitle_CHS, CP_ACP);
+	if (!title_CHT) title_CHT = wcs2cs_alloc(wtitle_CHT, CP_ACP);
+
 	memset(&si, 0, sizeof(si));
 	si.cb = sizeof(si);
 	memset(&pi, 0, sizeof(pi));
@@ -47,11 +49,11 @@ void OpenConfigTool(CPatchConfigDlg *dlg)
 		while (MsgWaitForMultipleObjects(1, &pi.hProcess, FALSE, 100, QS_ALLEVENTS) != WAIT_OBJECT_0) {
 			DoEvents();
 			if (GetPleaseWaitDlg()->m_hWnd == GetForegroundWindow()) {
-				EnumWindows(SetForegroundIfMatched, (LPARAM) &pi);
+				EnumThreadWindows(pi.dwThreadId, SetForegroundIfMatched, 0);
 			}
 		}
 		DWORD dwExitCode;
-		if (!GetExitCodeProcess(pi.hProcess, &dwExitCode) || dwExitCode != 0) {
+		if (!GetExitCodeProcess(pi.hProcess, &dwExitCode) || (dwExitCode != 0 && dwExitCode != 27)) {
 			GetPleaseWaitDlg()->MessageBox(STRTABLE(IDS_CONFIGTOOLERROR), STRTABLE(IDS_CONFIGTOOLERROR_TITLE), MB_ICONERROR);
 		}
 		CloseHandle(pi.hProcess);
